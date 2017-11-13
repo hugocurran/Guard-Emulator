ad54454fbc319301dd42ea3744175e0e487ccf9c
Peter Curran <Peter@currans.eu>
2017-11-13 11:08:46 +0000

    Add project files.

---
+++ Guard Emulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guard_Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
